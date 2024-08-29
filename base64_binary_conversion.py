0887f84f4f06143ca089dc1cea3b0ec3d7f6f5a5
aaditya2291 <ayyala.aditya.2291@gmail.com>
2024-08-30 03:53:49 +0530

    added

---
+++ base64_binary_conversion.py
@@ -0,0 +1,16 @@
+def base64_binary(data):
+    """
+        Takes the loaded json data as input. Then extracts the base64 string from it.
+        After the extraction this string will be converted binary hexadecimal format.
+        Finally returns the binary hexadecimal string.
+    """
+    
+    import base64
+    
+    index_idsr = data['message'][0].index('IDS-R|')
+    index_string = index_idsr + 6
+    base64_string = data['message'][0][index_string:]
+    
+    binary_hex_string = base64.b64decode(base64_string).hex()
+    
+    return binary_hex_string
